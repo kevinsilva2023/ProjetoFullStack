9f5cf05846e93380e06f03661153ff8da5036d3c
Kevin Silva <149127319+kevinsilva2023@users.noreply.github.com>
2024-06-17 23:07:09 -0300

    realizando algumas alterações de estrutura e responsabilidade

--- BackEnd/src/ProEventos.API/Controllers/EventosController.cs
+++ BackEnd/src/ProEventos.API/Controllers/EventosController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Domain;
+using ProEventos.Persistence;
 
 namespace ProEventos.API.Controllers
 {
@@ -14,8 +14,8 @@ namespace ProEventos.API.Controllers
 
     public class EventosController : ControllerBase
     {
-        private readonly DataContext _context;
-        public EventosController(DataContext context)
+        private readonly ProEventosContext _context;
+        public EventosController(ProEventosContext context)
         {
             _context = context;
         }
@@ -32,7 +32,7 @@ namespace ProEventos.API.Controllers
         {
             // uma rota que retorna filtrando o id
             return _context.Eventos.FirstOrDefault(
-                evento => evento.EventoId == id
+                evento => evento.Id == id
             );
         }
         [HttpPost]
